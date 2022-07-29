8e96dcc553ab846d3fdf70c50b120a80e637be27
Carlos Petit <ca.alberto.p@gmail.com>
2022-07-29 15:48:18 -0400

    wip; refactor

--- pruebaBiblio2.py
+++ pruebaBiblio2.py
@@ -1,176 +1,176 @@
 # Ingresar bibliotecas a una lista, y luego en cualquiera de esas bibliotecas, que se buscan por nombre, añadir libros.
-#Guardar los libros en una lista, dentro de la biblioteca seleccionada, y despues tener la opción de consultarlos
+# Guardar los libros en una lista, dentro de la biblioteca seleccionada, y despues tener la opción de consultarlos
 
 
-
-#crear clase biblioteca con sus atributos
-class Biblioteca():
-
-    #inicializar
-    def __init__(self,bibliotecas=[],nombre_bi="",libros=[],nombre_lib=""):
-        self.bibliotecas=[]
-        self.nombre_bi=nombre_bi
-        self.libros=libros
-        self.nombre_lib=nombre_lib
-
-    
-    #metodo str para imprimir
-    def __str__(self): 
-        b= f"Bibliotecas: \n"
-        for nombre_bi in self.bibliotecas:
-            b+=f"{nombre_bi}\n"
-        return b
-
-    #no se si esto esta correcto, que haya 2 return en un str
-        l= f"Libros: \n"
-        for nombre_lib in self.libros:
-            l+=f"{nombre_lib}\n"
-        return l
-
-    #metodo para añadir biblioteca
-
-    def anadirBiblio(self):
-        continuar=True
-        while continuar:
-            nombre_bi=input("Ingrese nombre de biblioteca: ")
-            biblio=Biblioteca(nombre_bi)
-            self.bibliotecas.append(biblio)            
-            respuesta=input("¿Desea agregar otra biblioteca? (s/n)")
-            if respuesta != "s" and respuesta != "S":
-                continuar=False
-                
-
-    #metodo para consultar las bibliotecas ingresadas a la lista
-    def consultarBiblio(self):
-
-        print("Bibliotecas")
-        #si hay bibliotecas que las muestre
-        if len(self.bibliotecas)==0:
-            print("No hay bibliotecas registradas")
-        else:
-            #ciclo para mostrar, no funciona.
-            #El error puede estar en el str, en el guardado de la biblio en la lista o acá
-            #los intente cambiar todos y nada me resulto
-            for b in self.bibliotecas:
-                print(f"{b}")
-
-    #metodo para añadir libro
-
-    def anadirLibro(self): 
-        if len(self.bibliotecas)==0:
-            print("No hay bibliotecas registradas") 
-        else:
-            continuar=True
-            while continuar:                
-                for bib in self.bibliotecas:                    
-                    select=input("Ingrese nombre de biblioteca donde ingresar libro: ")
-                    if select == bib.nombre_bib:
-                        nombre_lib=input("Ingrese libro: ")
-                        libro=Libro(nombre_lib)
-                        self.libros.append(libro)
-                    else:
-                        print("No está ingresada esa biblioteca")
-                        
-                respuesta=input("¿Desea agregar otro libro? (s/n)")
-                if respuesta != "s" and respuesta != "S":
-                    continuar=False
-                
-            #for bib in self.bibliotecas:
-             #   select=input("Ingrese nombre de biblioteca donde ingresar libro: ")
-              #  if select == bib.nombre_bi:
-               #     nombre_lib=input("Ingrese libro: ")
-                #    libro=Libro(nombre_lib)
-                 #   bib.libros.append(libro)
-                #else:
-                #    print("No está ingresada esa biblioteca")
-                    
-                    
-        #continuar=True
-       # while continuar:
-            #busca la biblioteca en la cual ingresar el libro
-          #  buscar_biblio=input("Ingrese nombre de biblioteca: ")
-            #recorre la lista
-           # for b in self.bibliotecas:
-                #si encuentra el nombre, pide ingresar libro (no se si esto está bien)
-              #  if b.nombre_bi==buscar_biblio:
-               #     nombre=input("Ingrese nombre de libro: ")
-               #     lib=Libro(nombre)
-               #     self.libros.append(lib)
-               #     for l in self.libros:
-                #        if l.nombre_lib==nombre:
-               #             print(l)
-               #             respuesta=input("¿Desea agregar otro libro? (s/n)")
-                #            if respuesta != "s" and respuesta != "S":
-               #                 continuar=False
-               # else:
-               #1     print("Biblioteca no encontrada")
-
-
-    def buscarLibro(self):
-        pass
-        
-
-
-
+# crear clase biblioteca con sus atributos
 
 
 class Libro():
-    def __init__(self,nombre_lib=""):
-        self.nombre_lib=nombre_lib
+    libros = []
 
-    
-    def __str__(self):
-       lib=self.nombre_lib
-       return lib
+    def __init__(self, nombre_lib=""):
+        if(nombre_lib != ""):
+            self.setLibro(nombre_lib)
 
-    
-#acá va el menu
-class Contenedor(Biblioteca):
-    OPC_ANADIR_BIBLIOTECA=1
-    OPC_CONSULTAR_BIBLIOTECA=2
-    OPC_ANADIR_LIBRO=3
-    OPC_BUSCAR_LIBRO=4
-    OPC_SALIR=5
-    
-    def __init__(self,nombre_bi="",bibliotecas=[]):
-        super().__init__(nombre_bi)
-        self.bibliotecas=bibliotecas
+# Se añade libros
+    def setLibro(self, nombre_lib):
+        self.libros.append(nombre_lib)
+
+    def getLibros(self):
+        return self.libros
+
+    def getLibro(self, nombre_lib):
+        for libro in self.libros:
+            if libro == nombre_lib:
+                return libro
+        return None
 
     def __str__(self):
-        b= f"Bibliotecas: \n"
-        for nombre_bi in self.bibliotecas:
-            b+=f"{nombre_bi}\n"
+        b = f"Libros: \n"
+        for libro in self.libros:
+            b += f"{libro}\n"
         return b
 
 
-    def menu(self):
-        continuar=True
-        while continuar:
-            print (f'''
-        {self.OPC_ANADIR_BIBLIOTECA} Añadir biblioteca
-        {self.OPC_CONSULTAR_BIBLIOTECA} Consultar biblioteca
-        {self.OPC_ANADIR_LIBRO} Añadir libro
-        {self.OPC_BUSCAR_LIBRO} Buscar libro
-        {self.OPC_SALIR} Salir
-        ''')
+class Biblioteca():
 
-            opc=int(input("Selecciona una opción: "))
-            if opc==self.OPC_ANADIR_BIBLIOTECA:            
-                Biblioteca.anadirBiblio(self)
-            elif opc==self.OPC_CONSULTAR_BIBLIOTECA:
-                Biblioteca.consultarBiblio(self)
-            elif opc==self.OPC_ANADIR_LIBRO:
-                Biblioteca.anadirLibro(self)
-            elif opc==self.OPC_BUSCAR_LIBRO:
-                Biblioteca.buscarLibro(self)
-            elif opc==self.OPC_SALIR:
-                continuar=False
-                print("Nos vemos")
+    # inicializar
+    def __init__(self, nombre_bi="", libros: Libro = []):
+        self.nombre_bi = nombre_bi
+        self.libros = libros
+
+    # metodo str para imprimir
+    def __str__(self):
+        b = f"Biblioteca: \n" + f"{self.nombre_bi}\n"
+        return b
+
+    def setLibros(self, libros):
+        self.libros = libros
+
+    def getLibros(self):
+        return self.libros
+
+    def getName(self):
+        return self.nombre_bi
+
+    def setName(self, nombre_bi):
+        self.nombre_bi = nombre_bi
+
+
+bibliotecas = []
+
+
+def main():
+    continuar = True
+    OPC_ANADIR_BIBLIOTECA = 1
+    OPC_CONSULTAR_BIBLIOTECA = 2
+    OPC_ANADIR_LIBRO = 3
+    OPC_BUSCAR_LIBRO = 4
+    OPC_SALIR = 5
+    #  declare a list of Class biblioteca
+
+    while continuar:
+        print(f'''
+            {OPC_ANADIR_BIBLIOTECA} Añadir biblioteca
+            {OPC_CONSULTAR_BIBLIOTECA} Consultar biblioteca
+            {OPC_ANADIR_LIBRO} Añadir libro
+            {OPC_BUSCAR_LIBRO} Buscar libro
+            {OPC_SALIR} Salir
+            ''')
+        opc = int(input("Selecciona una opción: "))
+        if opc == OPC_ANADIR_BIBLIOTECA:
+            añadeBiblioteca()
+        elif opc == OPC_CONSULTAR_BIBLIOTECA:
+            consultaBiblioteca()
+        elif opc == OPC_ANADIR_LIBRO:
+            añadirLibros()
+        elif opc == OPC_BUSCAR_LIBRO:
+            buscarLibro()
+        elif opc == OPC_SALIR:
+            continuar = False
+            print("Nos vemos")
+            break
+
+
+def consultaBiblioteca():
+    select = input(
+        "Ingrese nombre de biblioteca a buscar: ")
+    biblioteca: Biblioteca
+    for biblioteca in bibliotecas:
+        if(select == biblioteca.getName()):
+            print("Heureka! Se ha encontrado la biblioteca: ",
+                  biblioteca.getName())
+            break
+        else:
+            print("No se ha encontrado la biblioteca: ", biblioteca.getName())
+            break
+    return
+
+
+def buscarLibro():
+    select = input(
+        "Ingrese nombre de biblioteca donde buscar el libro: ")
+    bibli: Biblioteca
+    for bibli in bibliotecas:
+        print(select, bibli.getName())
+        if select == bibli.getName():
+            selectedBiblio = bibli
+            break
+        else:
+            print("No está ingresada esa biblioteca")
+            break
+    nombre_lib = input("Ingrese libro a consultar: ")
+    libro: Libro
+    if(selectedBiblio.libros.getLibro(nombre_lib) != None):
+        print("el libro " + selectedBiblio.libros.getLibro(nombre_lib) +
+              " se encuentra en la biblioteca " + selectedBiblio.getName())
+    else:
+        print("No se encontró el libro")
+    return
+
+
+def añadirLibros():
+    libros = Libro()
+    if len(bibliotecas) == 0:
+        print("No hay bibliotecas registradas")
+    else:
+        continuar = True
+        bibli: Biblioteca
+        x = 0
+        biblioFind = 0
+        selectedBiblio: Biblioteca
+        select = input(
+            "Ingrese nombre de biblioteca donde ingresar libro: ")
+        for bibli in bibliotecas:
+            print(bibli.getName(), select)
+            if select == bibli.getName():
+                selectedBiblio = bibli
+                biblioFind = x
                 break
+            else:
+                print("No está ingresada esa biblioteca")
+            x += 1
+        while continuar:
+            nombre_lib = input("Ingrese libro: ")
+            libros.setLibro(nombre_lib)
+            respuesta = input("¿Desea agregar otro libro? (s/n)")
+            if respuesta != "s" and respuesta != "S":
+                continuar = False
+        selectedBiblio.setLibros(libros)
+        bibliotecas[biblioFind] = selectedBiblio
+    return
 
 
-    
-bi=Contenedor()
-bi.menu()
+def añadeBiblioteca():
+    continuar = True
+    while continuar:
+        nombre_bi = input("Ingrese nombre de biblioteca: ")
+        biblioteca = Biblioteca(nombre_bi)
+        bibliotecas.append(biblioteca)
+        respuesta = input("¿Desea agregar otra biblioteca? (s/n)")
+        if respuesta != "s" and respuesta != "S":
+            continuar = False
+    return
 
-        
+
+main()
